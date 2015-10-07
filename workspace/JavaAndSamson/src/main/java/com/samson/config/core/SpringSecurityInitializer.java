2992d3bd7cd945d7f017ea32aa9d12f5bb986706
Samson Serhii <raffenkaf@gmail.com>
2015-10-07 11:26:38 +0300

    delete all

--- workspace/JavaAndSamson/src/main/java/com/samson/config/core/SpringSecurityInitializer.java
+++
@@ -1,8 +0,0 @@
-package com.samson.config.core;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer 
-	extends AbstractSecurityWebApplicationInitializer{
-	
-}
